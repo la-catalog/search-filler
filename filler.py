c1ecb3634d4e49e266d2db107a11501f63bf5d36
thiagola92 <thiagola92@gmail.com>
2022-06-19 10:42:31 -0300

    Refactor run.py

--- filler.py
+++ filler.py
@@ -1,5 +1,7 @@
-from aio_pika.channel import Channel
+from aio_pika.exchange import Exchange
+from aio_pika.message import Message
 from page_infra import Infra
+from rabbit_models.search_scraper import Body
 from structlog.stdlib import BoundLogger, get_logger
 
 
@@ -11,8 +13,12 @@ class Filler:
     async def setup(self):
         await self._infra.setup_search_database()
 
-    async def fill(self, channel: Channel, marketplace: str):
+    async def fill(self, exchange: Exchange, marketplace: str):
+        messages = []
         cursor = await self._infra.get_queries(marketplace=marketplace)
 
         async for doc in cursor:
+            Body()
             print(doc)
+
+        exchange.publish()
--- run.py
+++ run.py
@@ -1,11 +1,28 @@
 import asyncio
 import os
 
-from aio_pika import connect
+from aio_pika import Connection, connect
+from page_infra.options import get_marketplace_infra
+from structlog.stdlib import get_logger
 
 from filler import Filler
 
 filler = Filler(mongo_url=os.environ["MONGO_URL"])
+logger = get_logger().bind(deployment="search-fill")
+
+
+async def fill_queue(connection: Connection, marketplace: str) -> None:
+    infra = get_marketplace_infra(marketplace=marketplace, logger=logger)
+    channel = await connection.channel()
+
+    async with channel:
+        channel.publisher_confirms()
+        exchange = channel.default_exchange
+        queue = await channel.declare_queue(
+            name=infra.search_queue, durable=True, arguments={"x-max-priority": 10}
+        )
+
+        await filler.fill(exchange=exchange, marketplace=marketplace)
 
 
 async def main():
@@ -14,13 +31,10 @@ async def main():
     connection = await connect(os.environ["RABBIT_URL"])
 
     async with connection:
-        channel = await connection.channel()
-
-        async with channel:
-            await asyncio.gather(
-                filler.fill(channel=channel, marketplace="mercado_livre"),
-                filler.fill(channel=channel, marketplace="rihappy"),
-            )
+        await asyncio.gather(
+            fill_queue(connection=connection, marketplace="mercado_livre"),
+            fill_queue(connection=connection, marketplace="rihappy"),
+        )
 
 
 asyncio.run(main())
